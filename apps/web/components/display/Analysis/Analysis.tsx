a8490e521ba3c812ad45c28b2bee9d4636974a09
Szilárd Dóró <doroszilard@gmail.com>
2022-07-18 18:34:49 +0200

    added icon to date

--- apps/web/components/display/Analysis/Analysis.tsx
+++ apps/web/components/display/Analysis/Analysis.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Chip from '../../ui/Chip'
 
 export interface AnalysisProps {
@@ -25,11 +26,14 @@ export default function Analysis({
       className="grid grid-flow-row gap-2 p-4 border-2 rounded-md border-slate-200"
       key={id}
     >
-      <div>
+      <div className="grid grid-flow-row gap-1">
         <h2 className="text-lg font-bold">
           {[owner, repository, base_path].filter(Boolean).join('/')}
         </h2>
-        <p className="text-xs text-slate-500">{id}</p>
+
+        <p className="grid items-center justify-start grid-flow-col gap-1 text-xs text-slate-500">
+          {id}
+        </p>
       </div>
 
       <div className="grid items-center justify-start grid-flow-col gap-2">
@@ -40,11 +44,16 @@ export default function Analysis({
         )}
 
         {errorCount === 0 && warningCount === 0 && (
-          <Chip level="success">🎉 All good</Chip>
+          <Chip level="success">Success</Chip>
         )}
       </div>
 
-      <p className="text-sm font-medium">{formattedCreatedAt}</p>
+      <div className="grid justify-start grid-flow-col gap-4 opacity-80">
+        <p className="grid items-center justify-start grid-flow-col gap-1 text-xs">
+          <Image src="/icons/date.svg" alt="Calendar" width={16} height={16} />{' '}
+          {formattedCreatedAt}
+        </p>
+      </div>
     </section>
   )
 }
